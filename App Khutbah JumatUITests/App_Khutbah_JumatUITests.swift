0cc039271cee0508fe35d6bee83a401794da4332
Muhammad hidayatullah <muhammadhabibhidayatullah@macbook-pro-muhammad-373.local>
2018-11-22 09:33:53 +0700

    Initial Commit

---
+++ App Khutbah JumatUITests/App_Khutbah_JumatUITests.swift
@@ -0,0 +1,35 @@
+//
+//  App_Khutbah_JumatUITests.swift
+//  App Khutbah JumatUITests
+//
+//  Created by MUHAMMAD HABIB HIDAYATULLAH on 21/11/18.
+//  Copyright © 2018 habdev. All rights reserved.
+//
+import XCTest
+
+class App_Khutbah_JumatUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
